663c6f8e9a3bf8e1c4a7a079b635cdda8e8c9473
Rapkalin <raphael@myminicom.com>
2023-10-31 00:16:10 +0100

    Add user Policies

--- website/app/Http/Controllers/InstagramAuthController.php
+++ website/app/Http/Controllers/InstagramAuthController.php
@@ -14,6 +14,9 @@ class InstagramAuthController extends Controller
      */
     public function show ()
     {
+        // We check if the user is allowed to access the current feed
+        $this->authorize('view', [User::class, Auth::user()->id]);
+        
         $user = User::where('id', Auth::user()->id)->first(['name', 'email']);
         $profile = Profile::for($user->name);
         $instagramPosts = $profile?->feed(9);
