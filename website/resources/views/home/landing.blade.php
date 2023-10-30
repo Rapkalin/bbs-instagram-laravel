f22df036950d9d46cb292b99527c2fbfef0d78e9
Rapkalin <raphael@myminicom.com>
2023-10-31 00:19:48 +0100

    Cleaning

--- website/resources/views/home/landing.blade.php
+++ website/resources/views/home/landing.blade.php
@@ -1,14 +1,9 @@
-@php
-    use Illuminate\Support\Facades\Route as Route;
-@endphp
 @extends('layouts.app')
 
 @section('content')
 
     <main class="bbs-instagram-login">
-        {{--<a href="{{ $instagram_auth_url }}">Click to get Instgram permission</a>--}}
         <a href="{{ route('users.create') }}">Créer un compte</a>
         <a href="{{ route('auth.login') }}">Se connecter</a>
     </main>
-
-@stop
+@endsection
